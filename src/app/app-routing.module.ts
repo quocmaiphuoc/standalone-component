67d99e4ca4dcc6b628322a2eedd00c07e8043d44
quocmaiphuoc <33285658+quocmaiphuoc@users.noreply.github.com>
2023-03-19 16:27:45 +0700

    create new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DogsListComponent } from './dogs-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list' },
+  { path: 'list', component: DogsListComponent },
+  {
+    path: 'details/:index',
+    loadComponent: () =>
+      import('./dog-view.component').then((m) => m.DogViewComponent),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
